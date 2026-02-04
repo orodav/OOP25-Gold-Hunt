737b447fd41b320a618d3b11d9e4e00b9f82d28a
azzurraquattrini <azzurra.quattrini@studio.unibo.it>
2026-02-04 17:48:51 +0100

    Created interface Cell (missing some imports and javadoc comments)

---
+++ src/main/java/it/unibo/goldhunt/board/api/Cell.java
@@ -0,0 +1,29 @@
+// AZZU
+
+package it.unibo.goldhunt.board.api;
+
+import java.util.Optional;
+
+
+public interface Cell {
+
+    void reveal();
+
+    boolean isRevealed();
+    
+    void toggleFlag();
+    
+    boolean isFlagged();
+
+    int getAdjacentTraps();
+
+    void setAdjacentTraps();
+
+    boolean hasContent();
+
+    Optional<CellContent> getContent();
+
+    void setContent(CellContent content);
+
+    void removeContent();
+}
