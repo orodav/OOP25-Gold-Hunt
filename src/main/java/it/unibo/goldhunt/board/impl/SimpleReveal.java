411cbfe0399172bdc498a513d74bee425286a234
azzurraquattrini <azzurra.quattrini@studio.unibo.it>
2026-02-05 13:50:07 +0100

    Created class SimpleReveal

---
+++ src/main/java/it/unibo/goldhunt/board/impl/SimpleReveal.java
@@ -0,0 +1,17 @@
+package it.unibo.goldhunt.board.impl;
+
+import it.unibo.goldhunt.board.api.Board;
+import it.unibo.goldhunt.board.api.RevealStrategy;
+import it.unibo.goldhunt.engine.api.Position;
+
+public class SimpleReveal implements RevealStrategy {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void reveal(Board b, Position p) {
+        
+    }
+
+}
